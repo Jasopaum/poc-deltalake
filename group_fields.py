f1560de91ff7c7abe13c67290c8a517ae7976fb5
Jasopaum <jason.paumier@gmail.com>
2021-04-09 20:51:59 +0200

    First spark script to read from DB, group fields, write to DB

---
+++ group_fields.py
@@ -0,0 +1,31 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+
+# from pyspark.sql.functions import col, expr
+# from delta.tables import DeltaTable
+
+spark = (
+    SparkSession.builder.appName("quickstart")
+    .master("local[*]")
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+    .getOrCreate()
+)
+
+# Read DB
+df = spark.read.jdbc(
+    "jdbc:postgresql://localhost:5432/delta-source",
+    "fields",
+    properties={"user": "delta", "password": "delta", "driver": "org.postgresql.Driver"},
+)
+
+# Hacky but ok
+grouped_df = df.groupby("form_id").agg(F.min("date"), F.min("time"))
+
+# Write to DB
+grouped_df.write.jdbc(
+    url="jdbc:postgresql://localhost:5432/delta-source",
+    table="grouped_fields",
+    mode="overwrite",
+    properties={"user": "delta", "password": "delta", "driver": "org.postgresql.Driver"},
+)
